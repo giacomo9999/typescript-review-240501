26a4350c240e4930f40a058185cc25c5354e727d
giacomo9999 <gmarchesi888@gmail.com>
2024-05-01 16:59:37 -0400

    Reviewing the basics.

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-console.log("Type");
+function add(n1, n2) {
+    if (typeof number1 !== "number" || typeof number2 !== "number") {
+        throw new Error("Incorect input!");
+    }
+    return n1 + n2;
+}
+var number1 = "5";
+var number2 = 5.6;
+var result = add(number1, number2);
+console.log(result);
--- app.ts
+++ app.ts
@@ -1,1 +1,12 @@
-console.log("Type");
+function add(n1: number, n2: number) {
+  //   if (typeof number1 !== "number" || typeof number2 !== "number") {
+  //     throw new Error("Incorect input!");
+  //   }
+  return n1 + n2;
+}
+
+const number1 = 5;
+const number2 = 5.6;
+
+const result = add(number1, number2);
+console.log(result);
